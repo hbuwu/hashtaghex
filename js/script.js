e3c0c25163b4d35ffe13287815cd60f7116a676c
Alex <alexander.vanbets@gmail.com>
2020-12-25 14:20:15 +0100

    Add full button functionality and visibility, fix grid on portfolio items not working

--- js/script.js
+++ js/script.js
@@ -24,10 +24,16 @@ function showSlides() {
     setTimeout(showSlides, 10000); // Change image every 2 seconds
 }
 
-function portfolioSwitch(portfolioName) {
+function portfolioSwitch(event, portfolioName) {
     let item = document.getElementsByClassName("portfolioItem");
     for (let index = 0; index < item.length; index++) {
         item[index].style.display = "none";
     }
-    document.getElementById(portfolioName).style.display = "block";
+    let tabs = document.getElementsByClassName("tab");
+    for (let index = 0; index < tabs.length; index++) {
+        tabs[index].className = tabs[index].className.replace(" selectedButton", "");
+    }
+
+    document.getElementById(portfolioName).style.display = "grid";
+    event.currentTarget.className += " selectedButton";
 }
